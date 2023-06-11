54aebfc03c5c51009ffe1d96f7a97ddf1af9c4d1
Tejareddy <mandateja@Tejareddys-MacBook-Pro.local>
2023-06-11 11:17:34 -0500

    feat: add review feature

--- src/App.js
+++ src/App.js
@@ -6,9 +6,13 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './components/home/Home';
 import Header from './components/header/Header';
 import Trailer from './components/trailer/Trailer';
+import Reviews from './components/reviews/Reviews';
+import NotFound from './components/notFound/NotFound';
 
 function App() {
   const [movies, setMovies] = useState();
+  const [movie, setMovie] = useState();
+  const [reviews, setReviews] = useState();
 
   const getMovies = async() => {
     try {
@@ -17,6 +21,17 @@ function App() {
       setMovies(response.data);
     } catch (err) {
       console.error(err);
+    }
+  };
+
+  const getMovieData = async (movieId) => {
+    try {
+      const response = await api.get(`/api/v1/movies/${movieId}`);
+      const singleMovie = response.data;
+      setMovie(singleMovie);
+      setReviews(singleMovie.reviewIds);
+    } catch (err) {
+      console.log(err);
     }
   }
 
@@ -31,6 +46,11 @@ function App() {
         <Route path='/' element={<Layout/>}>
           <Route path='/' element={<Home movies={movies}/>}/>
           <Route path='/trailer/:ytTrailerId' element={<Trailer />} />
+          <Route path='/reviews/:movieId' element={
+            <Reviews getMovieData={getMovieData} movie={movie} reviews={reviews} setReviews={setReviews}/>
+            }
+          />
+          <Route path="*" element={<NotFound />}/>
         </Route>
       </Routes>
     </div>
