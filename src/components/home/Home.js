a4c6e1f5031f85bf637695e5dc1e83fccac4c878
Tejareddy <mandateja@Tejareddys-MacBook-Pro.local>
2023-06-11 22:17:12 -0500

    remove console.log statement

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Hero from '../hero/Hero';
 
 const Home = ({movies}) => {
-  console.log('movies array: ', movies);;
   return (
     movies && <Hero movies={movies}/>
   );
